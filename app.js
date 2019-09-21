1de49319963472ea96fcc0cdcee686c7ada05b9f
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-21 12:30:55 -0700

    mernstack init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello MERN'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
