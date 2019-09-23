aadd3977fa47aa1c2d774223acca503e9c8539ce
Ash Todd <ashztodd@gmail.com>
2019-09-23 12:52:24 -0700

    Fake dropdown activated

--- frontend/src/components/nav/nav_bar.jsx
+++ frontend/src/components/nav/nav_bar.jsx
@@ -3,14 +3,26 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropdown: false
+        }
 
+        this.handleHamburger = this.handleHamburger.bind(this);
+    }
+
+    handleHamburger() {
+        //fix this later so it works with the ui reducer
+        this.setState({dropdown: !this.state.dropdown})
+    }
 
     render() {
         return (
         <div className="nav-bar">
             <img className="logo" src="logo512.png"></img>
-            <div className="dropdown-container">
-                <i className="material-icons dropdown-button">menu</i>
+            <div className={this.state.dropdown ? "dropdown-container open" : "dropdown-container"}>
+                <i className="material-icons dropdown-button" onClick={this.handleHamburger}>menu</i>
                 <div className="dropdown">
                     <div className="user-links">
                         <div>Profile Link</div>
@@ -20,7 +32,7 @@ class NavBar extends React.Component {
                         <div>My Shoots Link</div>
                         <div>New Story Link</div>
                     </div>
-                    {/* { currentUser true */}
+                    {true ?
                         <div className="auth-links">
                             <div>
                                 Sign Out Link
