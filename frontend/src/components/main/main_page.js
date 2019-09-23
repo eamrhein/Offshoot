1b60406fb856780212da8800dea9cecd42bd27da
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-22 22:08:42 -0700

    Working root component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Offshoot</h1>
+        <footer>
+          Copyright &copy; 2019
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
