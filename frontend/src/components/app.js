782faf0a1b225533b1dd0678e91434bfbf39990d
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-24 11:08:13 -0700

    adds navbar to app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,14 +3,18 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import Register from './session/signup_form_container';
 import Login from './session/login_form_container';
+import NavBar from './nav/nav_bar_container';
 import MainPage from './main/main_page';
 
 const App = () => (
-  <Switch>
-    <AuthRoute exact path = '/register' component={Register} />
-    <AuthRoute exact path = '/login' component={Login} />
-    <AuthRoute exact path="/" component={MainPage} />
-  </Switch>
+  <div>
+    <NavBar />
+    <Switch>
+      <AuthRoute exact path = '/register' component={Register} />
+      <AuthRoute exact path = '/login' component={Login} />
+      <AuthRoute exact path="/" component={MainPage} />
+    </Switch>
+  </div>
 );
 
 export default App;
