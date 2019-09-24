9a65e6935c8a0aa05e244d96c6dee120429ff8ec
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-24 11:17:07 -0700

    fix app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,12 +3,10 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import Register from './session/signup_form_container';
 import Login from './session/login_form_container';
-import NavBar from './nav/nav_bar_container';
 import MainPage from './main/main_page';
 
 const App = () => (
   <div>
-    <NavBar />
     <Switch>
       <AuthRoute exact path = '/register' component={Register} />
       <AuthRoute exact path = '/login' component={Login} />
