cd52836cef73ea0eb4e3a7fffb6b423dd54ab686
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-23 17:47:52 -0700

    fixes misspelling

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
-const tweets = require('./routes/api/tweets');
 const bodyParser = require('body-parser');
 
 mongoose
