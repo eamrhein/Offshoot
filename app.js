ef56cdd825dc882bc3a2fa9c3765beef9714ccae
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-23 17:47:29 -0700

    fixes misspelling

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello MERN'));
 app.use('/api/users', users);
-app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
