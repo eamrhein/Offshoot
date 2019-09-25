d9fc7568c2e63744c1eac11ed5e918a13e4f06fe
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-25 10:49:38 -0700

    merge changes with master

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Route, Switch } from 'react-router-dom';
 
-import NavBarContainer from './nav/nav_bar_container';
+import NavBarContainer from './nav/nav_bar_container';1
 
 import Register from './session/signup_form_container';
 import Login from './session/login_form_container';
