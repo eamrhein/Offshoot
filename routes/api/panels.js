308b646c5e10f01d20e8a5fb013a4b8bcffd9682
Ash Todd <ashztodd@gmail.com>
2019-09-25 10:46:02 -0700

    Commit before pulling from master

--- routes/api/panels.js
+++ routes/api/panels.js
@@ -8,62 +8,63 @@ const validatePanel = require('../../validation/panel');
 // const { aws_access_key_id, aws_secret_access_key, sw3_bucket } = aws;
 
 // configure aws
-//aws.config.region = 'us-west-2';
+// aws.config.region = 'us-west-2';
 
 router
-    .get('/:id', (req, res) => {
-        const {errors, isValid} = validatePanel;
+  .get('/:id', (req, res) => {
+    const { errors, isValid } = validatePanel;
 
-        if (!isValid) res.status(422).json(errors);
+    if (!isValid) res.status(422).json(errors);
 
-        const { authorId, title, panelText, photoURL, parentId, rootId } = req.body;
+    // eslint-disable-next-line no-unused-vars
+    const { authorId, title, panelText, photoURL, parentId, rootId } = req.body;
 
-        const panel = Panel.findById(req.params.id);
+    const panel = Panel.findById(req.params.id);
 
-        res.json(panel);
-    })
-    .post('/', (req, res) => {
-        const { errors, isValid } = validatePanel;
+    res.json(panel);
+  })
+  .post('/', (req, res) => {
+    const { errors, isValid } = validatePanel;
 
-        if (!isValid) res.status(422).json(errors);
+    if (!isValid) res.status(422).json(errors);
 
-        const { authorId, title, panelText, photoURL, parentId, rootId } = req.body;
-        
-        const newPanel = new Panel({
-            authorId,
-            title,
-            panelText,
-            parentId,
-            rootId
-        });
+    // eslint-disable-next-line no-unused-vars
+    const { authorId, title, panelText, photoURL, parentId, rootId } = req.body;
 
-        newPanel.save()
-            .then(panel => res.json(panel))
-            .catch(err => console.log(err));
+    const newPanel = new Panel({
+      authorId,
+      title,
+      panelText,
+      parentId,
+      rootId
+    });
 
-        // const fileName = req.query['file-name'];
-        // const fileType = req.query['file-type'];
-        // const s3Params = {
-        //     Bucket: sw3_bucket,
-        //     Key: fileName,
-        //     Expires: 60,
-        //     ContenType: fileType,
-        //     ACL: 'pubic-read'
-        // };
+    newPanel.save()
+      .then(panel => res.json(panel))
+      .catch(err => console.log(err));
 
-        // s3Params.getSignedUrl('putObject', s3Params, (err, data) => {
-        //     if (err) {
-        //         console.log(err);
-        //         return res.end();
-        //     }
-        //     const returnData = {
-        //         signedRequest: data,
-        //         url: `https://${sw3_bucket}.s3.amazonaws.com/${fileName}`
-        //     };
-        //     res.write(JSON.stringify(returnData));
-        //     res.end();
-        // });
+    // const fileName = req.query['file-name'];
+    // const fileType = req.query['file-type'];
+    // const s3Params = {
+    //     Bucket: sw3_bucket,
+    //     Key: fileName,
+    //     Expires: 60,
+    //     ContenType: fileType,
+    //     ACL: 'pubic-read'
+    // };
 
-});
+    // s3Params.getSignedUrl('putObject', s3Params, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.end();
+    //     }
+    //     const returnData = {
+    //         signedRequest: data,
+    //         url: `https://${sw3_bucket}.s3.amazonaws.com/${fileName}`
+    //     };
+    //     res.write(JSON.stringify(returnData));
+    //     res.end();
+    // });
+  });
 
 module.exports = router;
