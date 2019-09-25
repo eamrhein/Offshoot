52582fe38ac0ba5b15950f019cc00078d2aaf580
DiogenestheDog <rguthrie14@gmail.com>
2019-09-25 11:59:16 -0700

    setting mongo in app.js

--- app.js
+++ app.js
@@ -6,14 +6,35 @@ const users = require('./routes/api/users');
 const panels = require('./routes/api/panels');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+require('dotenv').config();
 
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.log(err));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to the Database.');
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose Connection Error : ' + err);
+});
+
+if (process.env.NODE_ENV === 'production') {
+
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
