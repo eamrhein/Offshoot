3a5a3cc69918828ce493a07f9702f22976f513c7
Ash Todd <ashztodd@gmail.com>
2019-09-24 17:04:46 -0700

    Will someday render a list of branches

--- frontend/src/components/panel/show/panel_show.jsx
+++ frontend/src/components/panel/show/panel_show.jsx
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import Panel from '../panel';
+import RouteIndex from './routes/route_index';
 
 export class PanelShow extends Component {
     render() {
         return (
             <div className="panel-show">
                 <Panel panelId={this.props.match.params.panelId}/>
-                <div>route index placeholder</div>
+                <RouteIndex />
                 <div>comment index placeholder</div>
             </div>
+            
         )
     }
 }
