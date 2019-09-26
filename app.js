14d927178574dd55168d4a1e7e579a866e914a43
DiogenestheDog <rguthrie14@gmail.com>
2019-09-25 17:53:29 -0700

    fixed moonl;asjf;

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-// const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
+const path = require('path');
 const users = require('./routes/api/users');
 const panels = require('./routes/api/panels');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 //require('dotenv').config();
 
-// mongoose
-//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-  console.log('Connected to the Database.');
-});
-mongoose.connection.on('error', err => {
-  console.log('Mongoose Connection Error : ' + err);
-});
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+// mongoose.connection.once('open', () => {
+//   console.log('Connected to the Database.');
+// });
+// mongoose.connection.on('error', err => {
+//   console.log('Mongoose Connection Error : ' + err);
+// });
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
