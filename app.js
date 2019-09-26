70454f06491172ee973fe88a0c55e1eb272f5c4f
DiogenestheDog <rguthrie14@gmail.com>
2019-09-25 17:06:35 -0700

    heroku config

--- app.js
+++ app.js
@@ -22,13 +22,10 @@ mongoose.connection.on('error', err => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-
-
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
-  });
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
 }
 
 app.use(passport.initialize());
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://<DBUsername>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+}
