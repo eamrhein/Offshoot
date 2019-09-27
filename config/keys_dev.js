f40e7865efcb9d2cc172a0200da22b5aa1341ad5
DiogenestheDog <rguthrie14@gmail.com>
2019-09-26 17:33:47 -0700

    oopsies

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://aA-Dev:WeZNBtsnsJwHJDcu@cluster0-eu8re.mongodb.net/offshoot?retryWrites=true&w=majority',
-    secretOrKey: 'y7gQCHWiZLarr7',
-    aws_access_key_id: "AKIAZ3QJ4OZP4M5TIZ5B",
-    aws_secret_access_key: "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",
-    s3_bucket: "offshoot-dev"
-}
