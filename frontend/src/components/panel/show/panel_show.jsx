1bce13f5eb472e2b7f017b224af0279eac852b2e
Ash Todd <ashztodd@gmail.com>
2019-09-27 17:51:47 -0700

    Panels are now swipeable

--- frontend/src/components/panel/show/panel_show.jsx
+++ frontend/src/components/panel/show/panel_show.jsx
@@ -2,24 +2,37 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchPanel } from '../../../actions/panel_actions';
+import { useSwipeable, Swipeable } from 'react-swipeable';
 
 import Panel from '../panel';
 import BranchIndex from './branches/branch_index';
 
 export class PanelShow extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSwipe = this.handleSwipe.bind(this);
+    }
+
     componentDidMount() {
         this.props.fetchPanel(this.props.match.params.panelId);
+    }
+
+    handleSwipe() {
+        if (this.props.panel.parentId) {
+            this.props.history.push(`/panels/${this.props.panel.parentId}`);
+        }
     }
 
     render() {
 
         if (this.props.panel) {
             return (
-            <div className="panel-show">
-                <Panel panelId={this.props.match.params.panelId}/>            
-                <BranchIndex panelId={this.props.match.params.panelId}/>
-                <div>comment index placeholder</div>
-            </div>
+            <Swipeable onSwiped={this.handleSwipe} className="panel-show">
+                    <Panel panelId={this.props.match.params.panelId}/>            
+                    <BranchIndex panelId={this.props.match.params.panelId}/>
+                    <div>comment index placeholder</div>
+            </Swipeable>
             )
         } else {
             return (
