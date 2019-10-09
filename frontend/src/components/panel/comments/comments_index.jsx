bc5cf7adaefd945c8bdc0a04c2dd9e123f273913
Eric Amrhein <eamrhein@protonmail.ch>
2019-10-09 15:13:11 -0700

    fixes comment issues

--- frontend/src/components/panel/comments/comments_index.jsx
+++ frontend/src/components/panel/comments/comments_index.jsx
@@ -6,15 +6,28 @@ import CreateComment from './create_comment';
 
 
 const mSTP = (state, ownProps) => ({
-  panel: state.entities.panels[ownProps.match.params.panelId]
+  panel: state.entities.panels[ownProps.match.params.panelId],
+  currentUser: state.session.user
 })
 
 
 const CommentsIndex = (props) => {
   let { comments } = props.panel;
   let { authorId } = props.panel;
+
+  function createDelete(currentUser, authorId) {
+    debugger
+    console.log(currentUser)
+    if(currentUser.id ===  authorId) {
+      return(<button>Delete</button>)
+    }
+  }
+
   comments = comments.map((comment) => (
     <div key={comment.id}>
+      {
+        createDelete(props.currentUser, authorId)
+      }
       <div>
         Posted by {comment.username}  {timeSince(comment.timestamp)}  Ago
       </div>
